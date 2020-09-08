2f659f50122472ec2b4f6b2140b2aca236b12d28
Audrey Thomasson <audrey@macbook-pro.lan>
2020-09-08 12:00:47 -0500

    albis working

--- scripts/criminals/AlibiDialog.js
+++ scripts/criminals/AlibiDialog.js
@@ -3,22 +3,27 @@ const eventHub = document.querySelector(".container")
 
 eventHub.addEventListener("associatesClicked", event => {
     //display all associates for a criminal
-    debugger
 
     const targetCriminal = useCriminals().find(criminal => {
         return criminal.id === parseInt(event.detail.chosenCriminal)
         // parseInt turns a string into a number
     })
     const alibiTarget = document.querySelector(`.alibiDialog--${targetCriminal.id}`);
-
-    alibiTarget.innerHTML = `${
-        targetCriminal.known_associates.map(associate => {
-            return `
-           <h4>${associate.name}</h4>
-           <div>${associate.alibi}</div> 
-            `
-        }).join("")
-    }`
+    const hTarget = alibiTarget.querySelector("h4")
+    
+    if (alibiTarget.contains(hTarget)) {
+        
+        alibiTarget.innerHTML = ""
+    } else {
+        alibiTarget.innerHTML = `${
+            targetCriminal.known_associates.map(associate => {
+                return `
+                <h4>${associate.name}</h4>
+                <div>${associate.alibi}</div> 
+                `
+            }).join("")
+        }`
+    }
 })
 
 
--- scripts/criminals/criminalProvider.js
+++ scripts/criminals/criminalProvider.js
@@ -7,7 +7,6 @@ export const getCriminals = () => {
         .then(
             parsedCriminals => {
                 console.table(parsedCriminals)
-                debugger
                 criminals = parsedCriminals
             }
         )
